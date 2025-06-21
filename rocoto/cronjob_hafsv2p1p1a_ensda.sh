b1ca0b653ee1d2c1997229795a8d99221050fe18
Bin Liu <Bin.Liu@noaa.gov>
2025-06-21 19:26:37 +0000

    - Update default atm_init PE layout settings  - Update and unify HAFS supported platforms' site entity files  - Enable properly setting nsst related options for HAFS ensemble forecast

--- rocoto/cronjob_hafsv2p1p1a_ensda.sh
+++ rocoto/cronjob_hafsv2p1p1a_ensda.sh
@@ -8,12 +8,12 @@ source ${HOMEhafs}/ush/hafs_pre_job.sh.inc
 cd ${HOMEhafs}/rocoto
 EXPT=$(basename ${HOMEhafs})
 #opts="-t -s sites/${WHERE_AM_I:-wcoss2}.ent -f"
-#opts="-t -f"
-opts="-t -s sites/ursa_hafsv2p1p1.ent -f"
+opts="-t -f"
 #===============================================================================
-# 
-## Technical testing for Helene 09L2024
+# HAFSv2.1.1A configuration including 4DEnVar with 3-hourly 40 coldstart HAFS
+# ensembles (instead of 80 GDAS ensembles).
  confopts="config.EXPT=${EXPT} config.SUBEXPT=${EXPT}_hfsa_ensda ../parm/hafsv2p1p1a_ensda.conf "
+ # Technical testing for Helene 09L2024
  ./run_hafs.py ${opts} 2024092406-2024092412 09L HISTORY ${confopts} \
     config.NHRS=126 config.scrub_work=no config.scrub_com=no config.run_emcgraphics=yes
 #===============================================================================
